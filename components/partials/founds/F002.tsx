ff0ecbe67a28d8a2ac136b6ccce0b875d8fbdc1d
shuta13 <hollystarsun@gmail.com>
2020-04-13 03:31:42 +0900

    wip: f002

---
+++ components/partials/founds/F002.tsx
@@ -0,0 +1,12 @@
+// refer
+// https://threejs.org/examples/?q=webgl_buffergeometry#webgl_buffergeometry_drawrange
+
+import React from "react";
+
+const F002: React.FC = () => {
+  return (
+    <div className="container"></div>
+  )
+}
+
+export default F002;
--- pages/founds/002.tsx
+++ pages/founds/002.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import dynamic from "next/dynamic";
+
+const F002 = dynamic(
+  () => import("../../components/partials/founds/F002"),
+  { ssr: false }
+)
 
 const _002: React.FC = () => {
-  return <div className="container"></div>;
+  return <F002 />;
 };
 
 export default _002;
