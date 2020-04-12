1d8c2f2dc7ce99a10f2de32be013162fb84be81b
shuta13 <hollystarsun@gmail.com>
2020-04-13 05:14:18 +0900

    fix: add comment

--- components/partials/founds/F002.tsx
+++ components/partials/founds/F002.tsx
@@ -42,8 +42,8 @@ type AnimateParams = {
 
 //
 
-const maxParticleCount = 1000;
-const particleCount = 800;
+const maxParticleCount = 100;
+const particleCount = 100;
 const r = 800;
 const rHalf = 800 / 2;
 const particlesData: Array<{
@@ -90,10 +90,12 @@ const animate = ({
   for (let i = 0; i < particleCount; i++) {
     const particleData = particlesData[i];
 
+    // パーティクルの位置更新
     particlePositions[i * 3] += particleData.velocity.x;
     particlePositions[i * 3 + 1] += particleData.velocity.y;
     particlePositions[i * 3 + 2] += particleData.velocity.z;
 
+    // 正方形の壁に内面にぶつかった時反射する処理
     if (particlePositions[i * 3] < -rHalf || particlePositions[i * 3] > rHalf) {
       particleData.velocity.x = -particleData.velocity.x;
     }
@@ -109,6 +111,8 @@ const animate = ({
     ) {
       particleData.velocity.z = -particleData.velocity.z;
     }
+
+    // これよくわからん、手の数の制限?
     if (
       effectController.limitConnections &&
       particleData.numConnections >= effectController.maxConnections
@@ -116,6 +120,7 @@ const animate = ({
       continue;
     }
 
+    // 線の情報更新
     for (let j = i + 1; j < particleCount; j++) {
       const particleDataB = particlesData[j];
       if (
@@ -124,28 +129,37 @@ const animate = ({
       ) {
         continue;
       }
+      // 距離
       const dx = particlePositions[i * 3] - particlePositions[j * 3];
       const dy = particlePositions[i * 3 + 1] - particlePositions[j * 3 + 1];
       const dz = particlePositions[i * 3 + 2] - particlePositions[j * 3 + 2];
       const dist = Math.sqrt(dx * dx + dy * dy + dz * dz);
+
+      // 線をつなぐ処理
       if (dist < effectController.minDistance) {
+        // つなぐ本数が増える
         particleData.numConnections++;
         particleDataB.numConnections++;
 
+        // 線の色のeasing、距離が離れる程に0に漸近する
         const alpha = 1.0 - dist / effectController.minDistance;
 
+        // 1つ目の頂点の位置
         positions[vertexPos++] = particlePositions[i * 3];
         positions[vertexPos++] = particlePositions[i * 3 + 1];
         positions[vertexPos++] = particlePositions[i * 3 + 2];
 
+        // 2つ目の頂点、つなぎ先の位置
         positions[vertexPos++] = particlePositions[j * 3];
         positions[vertexPos++] = particlePositions[j * 3 + 1];
         positions[vertexPos++] = particlePositions[j * 3 + 2];
 
+        // 1つ目の頂点の色
         colors[colorPos++] = alpha;
         colors[colorPos++] = alpha;
         colors[colorPos++] = alpha;
 
+        // 2つ目の頂点、つなぎ先の色
         colors[colorPos++] = alpha;
         colors[colorPos++] = alpha;
         colors[colorPos++] = alpha;
@@ -185,6 +199,7 @@ const F002: React.FC = () => {
       return;
     }
 
+    // init
     const scene = new Scene();
     const camera = new PerspectiveCamera(
       45,
@@ -193,29 +208,37 @@ const F002: React.FC = () => {
       4000
     );
     camera.position.z = 1750;
-
     const group = new Group();
     scene.add(group);
+
+    // 枠組みの正方形
     const helper = new BoxHelper(new Mesh(new BoxBufferGeometry(r, r, r)));
     const h = helper.material as any;
-    h.color.setHex(0x101010);
+    h.color.setHex(0xff1111);
     h.blending = AdditiveBlending;
     h.transparent = true;
     group.add(helper);
 
+    // 関節部分のinit
+    // Float32Arrayでmax^2確保(パーティクル1つから最大4本手が生える)
     const segments = maxParticleCount * maxParticleCount;
     const positions = new Float32Array(segments * 3);
     const colors = new Float32Array(segments * 3);
+  
+    // 関節部分の図形
     const pMaterial = new PointsMaterial({
-      color: 0xffffff,
+      color: 0xff1111,
       size: 3,
       blending: AdditiveBlending,
       transparent: true,
       sizeAttenuation: false,
     });
     const particles = new BufferGeometry();
+    // パーティクルの位置max*3確保(パーティクルの最大数分のx,y,zで位置を確定)
     const particlePositions = new Float32Array(maxParticleCount * 3);
 
+    // パーティクルの位置をランダムに決める
+    // 正方形の真ん中を基準
     for (let i = 0; i < maxParticleCount; i++) {
       const x = Math.random() * r - r / 2;
       const y = Math.random() * r - r / 2;
@@ -226,6 +249,9 @@ const F002: React.FC = () => {
       particlePositions[i * 3 + 2] = z;
 
       particlesData.push({
+        // 3次元ベクトル
+        // -1 ~ 1の範囲にする、正規化みたいな感じ
+        // バラバラに動かすための重み?
         velocity: new Vector3(
           -1 + Math.random() * 2,
           -1 + Math.random() * 2,
@@ -235,15 +261,18 @@ const F002: React.FC = () => {
       });
     }
 
+    // 線を引く、paticleCountは頂点数
     particles.setDrawRange(0, particleCount);
+    // よくわからん : https://developer.mozilla.org/ja/docs/Web/API/WebGLRenderingContext/bufferData っぽい
     particles.setAttribute(
       "position",
       new BufferAttribute(particlePositions, 3).setUsage(DynamicDrawUsage)
     );
-
+    // 頂点を表示 : https://threejs.org/docs/#api/en/objects/Points
     const pointCloud = new Points(particles, pMaterial);
     group.add(pointCloud);
 
+    // 線のジオメトリー作成
     const geometry = new BufferGeometry();
     geometry.setAttribute(
       "position",
@@ -253,22 +282,27 @@ const F002: React.FC = () => {
       "color",
       new BufferAttribute(colors, 3).setUsage(DynamicDrawUsage)
     );
+    // リアルに跳ね返らせる演算
     geometry.computeBoundingSphere();
     geometry.setDrawRange(0, 0);
 
+    // 線のマテリアル作成
     const material = new LineBasicMaterial({
       vertexColors: true,
       blending: AdditiveBlending,
       transparent: true,
     });
 
+    // 線のメッシュ
     const linesMesh = new LineSegments(geometry, material);
+    // 線描画
     group.add(linesMesh);
 
     const renderer = new WebGLRenderer({ canvas: canvas, antialias: true });
     renderer.setClearColor("#1d1d1d");
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
+    // 枠組みの正方形の色付け?
     renderer.outputEncoding = sRGBEncoding;
     renderer.render(scene, camera);
 
