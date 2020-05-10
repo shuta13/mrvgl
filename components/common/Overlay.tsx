751d748df92c5081b6237a9e0ce82693ce6ac779
shuta13 <hollystarsun@gmail.com>
2020-05-10 21:56:43 +0900

    fix

---
+++ components/common/Overlay.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Overlay: React.FC = () => {
+  return (
+    <div className="Overlay">
+      <div className="OverlayText">Sorry, Coming Soon...</div>
+    </div>
+  )
+}
