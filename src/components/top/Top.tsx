fc6fa84242be6c68c967bb8bfb689c2d29708fc2
shuta13 <hollystarsun@gmail.com>
2021-12-13 19:05:06 +0900

    fix: color palette

--- src/components/top/Top.tsx
+++ src/components/top/Top.tsx
@@ -10,8 +10,8 @@ export const Top: React.FC = () => {
         {archives
           .filter((archive) => archive.id !== '000')
           .map((archive) => (
-            <Link href={`/archive/${archive.id}`}>
-              <li key={archive.id} className={styles.list}>
+            <Link href={`/archive/${archive.id}`} key={archive.id}>
+              <li className={styles.list}>
                 <a
                   className={
                     parseInt(archive.id) % 2 === 1
